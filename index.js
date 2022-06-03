8bfb9af89f47cf8a4faae8864e02c0024bb7aaac
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-06-04 02:08:18 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const getCardsArrayExpDate = (json, date) => {
+  const datePattern = /^(0?[1-9]|[12][0-9]|3[01])\-(0?[1-9]|1[012])\-\d{4}$/;
+  const isValidDate = datePattern.test(date.trim());
+  if (!isValidDate) {
+    return null;
+  }
+
+  const normalizeDate = (rawDate) =>
+    new Date(rawDate.split('-').reverse().join('-'));
+
+  const cards = json.CardInfo;
+  const filteredCards = cards.filter(
+    (card) => normalizeDate(card.ExpDate) > normalizeDate(date),
+  );
+
+  if (filteredCards.length === 0) {
+    return null;
+  }
+
+  return filteredCards.map((card) => {
+    const cardNumber = card.CardNumber;
+
+    return cardNumber.substring(cardNumber.length - 4);
+  });
+};
